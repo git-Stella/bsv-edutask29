d9c6e0ef646e98c2d9a9b4ef57b6591da8484984
Stella <meniallight@hotmail.com>
2026-04-10 13:33:26 +0200

    wrote unit tests

---
+++ backend/test/unit/test_getUserByEmail.py
@@ -0,0 +1,49 @@
+import pytest
+from src.controllers.usercontroller import UserController
+import unittest.mock as mock
+
+#I dont have to test dao
+#So I can just tell the dao to return that it does not exist
+#Just look at how the dao
+
+#@pytest.fixture
+#def userController():
+#    mockedDao = mock.MagicMock()
+    #emailList = [{'email': 'test@mail.com'}]
+    #Ask how do I get a specific set of values that it will return when asked on mock
+#    mockedDao.find.return_value = {'email': email}
+#    mockedController = UserController(dao=mockedDao)
+#    return mockedController
+
+@pytest.mark.unit
+def test_get_user_by_email_invalid():
+    mockedDao = mock.MagicMock()
+    mockedController = UserController(dao=mockedDao)
+    #validationRes = mockedController.get_user_by_email('test')
+    #assert validationRes == ValueError
+    with pytest.raises(ValueError):
+        validationRes = mockedController.get_user_by_email('test')
+
+#email valid but does not exist
+@pytest.mark.unit
+def test_get_user_by_email_valid_no_exists():
+    mockedDao = mock.MagicMock()
+    mockedController = UserController(dao=mockedDao)
+    mockedDao.find.return_value = Exception
+    #Was not quite sure how to check if an exception is raised...
+    #But here it says if one is raised then nothing will go wrong
+    #If it does not get raised it asserts something that is wrong.
+    try:
+        validationRes = mockedController.get_user_by_email('correct@mail.coop')
+        assert True is False
+    except Exception as e:
+        assert True is True
+
+#email valid and does exist
+@pytest.mark.unit
+def test_get_user_by_email_valid_exists():
+    mockedDao = mock.MagicMock()
+    mockedController = UserController(dao=mockedDao)
+    mockedDao.find.return_value = ['correct@mail.coop']
+    validationRes = mockedController.get_user_by_email('correct@mail.coop')
+    assert validationRes == 'correct@mail.coop'
