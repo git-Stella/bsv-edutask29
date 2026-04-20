587d2496f2f8e4c3a6ef25a35529d0535517fe47
Stella <meniallight@hotmail.com>
2026-04-20 11:43:13 +0200

    fixed mongourl

--- backend/src/util/dao.py
+++ backend/src/util/dao.py
@@ -29,7 +29,7 @@ class DAO:
         # connect to the MongoDB and select the appropriate database
         print(
             f'Connecting to collection {collection_name} on MongoDB at url {MONGO_URL}')
-        client = pymongo.MongoClient(MONGO_URL, username='root', password='root')
+        client = pymongo.MongoClient(MONGO_URL)
         database = client.edutask
 
         # create the collection if it does not yet exist
