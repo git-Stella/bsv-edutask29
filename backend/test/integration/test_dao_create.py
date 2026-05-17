f019a9295e2b3691591d65b011bc80d3610823b4
Stella <meniallight@hotmail.com>
2026-05-17 16:07:01 +0200

    fixed some stuff

--- backend/test/integration/test_dao_create.py
+++ backend/test/integration/test_dao_create.py
@@ -4,11 +4,29 @@ import json
 import os
 #from unittest.mock import patch, MagicMock
 import pymongo
+from bson import json_util
+from bson.objectid import ObjectId
 
 @pytest.fixture
 def database():
     #setup
     #using todo cause it is the simplest in static/validators
+    mockTodo = {
+        "$jsonSchema": {
+            "bsonType": "object",
+            "required": ["description"],
+            "properties": {
+                "description": {
+                    "bsonType": "string",
+                    "description": "the description of a todo must be determined",
+                    "uniqueItems": true
+                }, 
+                "done": {
+                    "bsonType": "bool"
+                }
+            }
+        }
+    }
     theDao = DAO('todo')
     yield theDao
     #teardown
@@ -19,17 +37,20 @@ def database():
 #def test_create_example(database):
 #    assert database
 
+@pytest.mark.integration
 def test_valid_key_val_pair(database):
     data = {'description': 'words'}
     res = database.create(data)
     assert '_id' in res
     assert isinstance(res['_id']['$oid'], str)
 
+@pytest.mark.integration
 def test_invalid_key(database):
     with pytest.raises(pymongo.errors.WriteError):
         data = {'willy_wonka': 'words'}
         database.create(data)
 
+@pytest.mark.integration
 def test_invalid_type(database):
     with pytest.raises(pymongo.errors.WriteError):
         data = {'description': 1}
